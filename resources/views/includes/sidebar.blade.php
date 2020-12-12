6c4d79c4d9dd104a141196523c26b67b4785cf72
suad alwajeeh <sd1.alwajeeh1@gmail.com>
2020-12-12 06:30:04 +0300

    edit src

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -2,7 +2,7 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{ asset('assets/dist/img/user8-128x128.jpg) }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .9;background-color: white;">
+      <img src={{asset('assets/img/logo.png')}} alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .9;background-color: white;">
       <span class="brand-text font-weight-light">JUBARI TRAVEL</span>
     </a>
 
@@ -11,7 +11,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('assets/dist/img/user8-128x128.jpg') }}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset('assets/img/user8-128x128.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">ADMIN NAME</a>
